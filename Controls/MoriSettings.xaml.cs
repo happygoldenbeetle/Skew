87d0cf301bca9a77b869becda45353540e525b1a
happygoldenbeetle <cartoontube1215@gmail.com>
2026-06-20 15:47:04 +0500

    Implement full Mac-style Settings layout in WinUI

--- Controls/MoriSettings.xaml.cs
+++ Controls/MoriSettings.xaml.cs
@@ -2,6 +2,7 @@ using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Input;
 using Mori.Models;
+using System.ComponentModel;
 
 namespace Mori.Controls;
 
@@ -13,6 +14,15 @@ public sealed partial class MoriSettings : UserControl
     public MoriSettings()
     {
         this.InitializeComponent();
+        Settings.PropertyChanged += Settings_PropertyChanged;
+    }
+
+    private void Settings_PropertyChanged(object sender, PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName == nameof(Settings.SearchEngine))
+        {
+            Bindings.Update();
+        }
     }
 
     private void Done_Click(object sender, RoutedEventArgs e)
@@ -24,4 +34,19 @@ public sealed partial class MoriSettings : UserControl
     {
         Store.SettingsVisible = false;
     }
+
+    // Two-Way binding helpers
+    public int NewTabBehaviorIndex => (int)Settings.NewTabBehavior;
+    public void SetNewTabBehaviorIndex(int index) => Settings.NewTabBehavior = (NewTabBehavior)index;
+
+    public int SearchEngineIndex => (int)Settings.SearchEngine;
+    public void SetSearchEngineIndex(int index) => Settings.SearchEngine = (SearchEngine)index;
+
+    public int ThemeIndex => (int)Settings.Theme;
+    public void SetThemeIndex(int index) => Settings.Theme = (ElementTheme)index;
+
+    public int SidebarPositionIndex => (int)Settings.SidebarPosition;
+    public void SetSidebarPositionIndex(int index) => Settings.SidebarPosition = (SidebarPosition)index;
+
+    public Visibility CustomSearchVisibility => Settings.SearchEngine == SearchEngine.Custom ? Visibility.Visible : Visibility.Collapsed;
 }
